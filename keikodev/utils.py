b9f50aef45f3f030e7be9d198acc0a9abcf55002
Albert García <spv4ever@gmail.com>
2024-01-26 22:03:27 +0100

    corrección para quitar el botón que necesitas backend

--- keikodev/utils.py
+++ keikodev/utils.py
@@ -83,36 +83,3 @@ kodi_pc_meta = [
     {"name": "og:description", "content": kodi_pc_description},
 ]
 kodi_meta.extend(_meta)
-
-class AlertDialogState(rx.State):
-    show: bool = False
-
-    def change(self):
-        self.show = not (self.show)
-
-
-def alertdialog_example():
-    return rx.vstack(
-        rx.button(
-            "Show Alert Dialog",
-            on_click=AlertDialogState.change,
-        ),
-        rx.alert_dialog(
-            rx.alert_dialog_overlay(
-                rx.alert_dialog_content(
-                    rx.alert_dialog_header("Confirm"),
-                    rx.alert_dialog_body(
-                        "Do you want to confirm example?"
-                    ),
-                    rx.alert_dialog_footer(
-                        rx.button(
-                            "Close",
-                            on_click=AlertDialogState.change,
-                        )
-                    ),
-                )
-            ),
-            is_open=AlertDialogState.show,
-        ),
-        width="100%",
-    )
