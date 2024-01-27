04a02dfaece72933ac96e5e425fe449d4ccfebfc
Albert García <spv4ever@gmail.com>
2024-01-27 12:49:17 +0100

    build antiguo

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://keikodev-production.up.railway.app reflex export --frontend-only
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
