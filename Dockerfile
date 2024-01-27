576fc81ca19465aa7d25b771061da8f48a4d6e04
Albert García <spv4ever@gmail.com>
2024-01-27 12:16:26 +0100

    api url

--- Dockerfile
+++ Dockerfile
@@ -12,3 +12,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 CMD reflex run --env prod --backend-only
 
+
--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only
+API_URL=https://keikodev-production.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
