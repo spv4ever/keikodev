580ce1ec7200246354967e4869007807cdf9281c
Albert García <spv4ever@gmail.com>
2024-01-27 13:59:00 +0100

    cors de nuevo

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="keikodev",
-#    cors_allowed_origins=[
-#        "https://keikodev.es",
-#        "https://keikodev.vercel.app",
-#        "http://localhost:3000"
-#    ]
+    cors_allowed_origins=[
+        "https://keikodev.es",
+        "https://keikodev.vercel.app",
+        "http://localhost:3000"
+    ]
 )
