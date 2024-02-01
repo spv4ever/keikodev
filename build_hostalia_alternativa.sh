a49e563f27dfae81540b8dee74237ddc5ce9de5f
Albert García <spv4ever@gmail.com>
2024-02-01 19:20:23 +0100

    poner al dia

--- build_hostalia_alternativa.sh
+++ build_hostalia_alternativa.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://api.keikodev.es reflex export --frontend-only
+API_URL=https://keikodev-dev-gaht.1.ie-1.fl0.io reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
