319d8a4f619de5ce50591c80a2eb1ecf95e8a500
spv4ever <spv4ever@gmail.com>
2024-02-05 17:32:35 +0100

    test base de datos

--- keikodev/api/db.py
+++ keikodev/api/db.py
@@ -1,2 +1,14 @@
-import reflex as rx
+import dotenv
+import os
+import requests
+from urllib.parse import quote_plus
+
+dotenv.load_dotenv()
+DBUSER = os.environ.get("DBUSER")
+DBPASSWORD = os.environ.get("DBPASSWORD")
+
+escaped_username = quote_plus(DBUSER)
+escaped_password = quote_plus(DBPASSWORD)
+
+#db_url=f"mysql://{escaped_username}:{escaped_password}@PMYSQL101.dns-servicio.com:3306/10252826_keikodeves"
 
--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,17 @@
 import reflex as rx
+import dotenv
+import os
+from urllib.parse import quote_plus
+import mysql.connector
 
+
+
+dotenv.load_dotenv()
+DBUSER = os.environ.get("DBUSER")
+DBPASS = os.environ.get("DBPASS")
+
+escaped_username = quote_plus(DBUSER)
+escaped_password = quote_plus(DBPASS)
 
 
 config = rx.Config(
@@ -9,5 +21,22 @@ config = rx.Config(
         "https://keikodev.vercel.app",
         "http://localhost:3000"
     ],
-    #db_url="mysql://adminkeikodeves:#######--@PMYSQL101.dns-servicio.com:3306/10252826_keikodeves"
+  #  db_url="mysql://{escaped_username}:{escaped_password}@PMYSQL101.dns-servicio.com:3306/10252826_keikodeves"
 )
+
+
+configdb = mysql.connector.connect(
+    host="PMYSQL101.dns-servicio.com",
+    user=DBUSER,
+    password=DBPASS,
+    database="10252826_keikodeves"
+
+)
+
+cursor = configdb.cursor()
+cursor.execute("SELECT * FROM nasa_imagenes")
+result = cursor.fetchall()
+print(result)
+
+cursor.close()
+configdb.close()
