061415bd0b1c53c7e156922b02f723e7c4a1042a
Albert García <spv4ever@gmail.com>
2024-02-22 21:26:48 +0100

    0.4.1

--- rxconfig.py
+++ rxconfig.py
@@ -10,7 +10,7 @@ DBPASS = os.environ.get("DBPASS")
 config = rx.Config(
     app_name="keikodev",
     api_url="https://api.keikodev.es",
-    db_url="mysql+mysqlconnector://{DBUSER}:{DBPASS}@PMYSQL101.dns-servicio.com:3306/10252826_keikodeves",
+    db_url=f"mysql+mysqlconnector://{DBUSER}:{DBPASS}@PMYSQL101.dns-servicio.com:3306/10252826_keikodeves",
     cors_allowed_origins=[
         "https://keikodev.es",
         "https://keikodev.vercel.app",
